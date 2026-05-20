5657f2f825b5c21f4692caadf2cbf45ed75137dc
Ray001-sudo <bensonray25@gmail.com>
2026-05-20 18:38:58 +0300

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies
+npm install
+
+# Ensure the Puppeteer cache directory exists
+PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+mkdir -p $PUPPETEER_CACHE_DIR
+
+# Install Puppeteer and download Chrome
+npx puppeteer browsers install chrome
